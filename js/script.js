e9f8e5d6e7e8c2788bbd69ee5c89d5bbb8850f03
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2020-07-11 19:11:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        
+        speed: 1500,
+       /*  adaptiveHeight: true, */
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron_left_solid_980.png" ></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron_right_solid_982.png" ></button>' 
+        
+      });
+  });
+          
